77acc148cf2924c925a9b67c54a186aa99cf581f
Manu200162 <manli200134@gmail.com>
2022-03-08 22:01:41 -0400

    Settings page finished

--- src/test/java/pages/ProjectCUD.java
+++ src/test/java/pages/ProjectCUD.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class ProjectCUD {
 
     //CREATE
-    public String nameProj="UPB"+new Date().getTime();
+    public String nameProj="MANUEL"+new Date().getTime();
     public Button addNewProjectButton = new Button(By.xpath("//td[text()='Add New Project']"));
     public TextBox nProjTxtBox = new TextBox(By.id("NewProjNameInput"));
     public Button saveNProject = new Button(By.id("NewProjNameButton"));
@@ -23,7 +23,7 @@ public class ProjectCUD {
     public Button optionsProjectButton = new  Button(By.xpath("//div[@style=\"display: block;\"]/img"));
     public Button updateProject= new Button(By.xpath("//ul[contains(@style,'block')]/li/a[text()='Edit']"));
     public TextBox updateProjTxtBox = new TextBox(By.xpath("//td/div/input[@id='ItemEditTextbox']"));
-    public String newNameProj="upb"+new Date().getTime()+"update";
+    public String newNameProj="manuel"+new Date().getTime()+"update";
     public Button saveUpdteButton = new Button(By.xpath("//td/div/img[@id=\"ItemEditSubmit\"]"));
     public Label actualProjectUpdate= new Label(By.xpath("//li[last()]//td[text()='"+newNameProj+"']"));
 
